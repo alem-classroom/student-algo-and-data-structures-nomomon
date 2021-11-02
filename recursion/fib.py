9afdcdd11125567430c2e6a7690e53d93c0dc681
Mansur Nurmukhambetov <56600275+nomomon@users.noreply.github.com>
2021-11-02 18:37:18 +0100

    fib

--- recursion/fib.py
+++ recursion/fib.py
@@ -1,2 +1,12 @@
 def fib(num):
     # return n-th Fibonacci number
+
+    # base case: n = 0
+    if(num == 0):
+        return 1
+    # base case: n = 1
+    if(num == 1):
+        return 1
+    
+    # recursion
+    return fib(num - 1) + fib(num - 2)
